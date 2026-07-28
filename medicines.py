bbbf0e92337a27f1b0ea8f70eef670b82e3f527e
hiralal <hiralal.joseph@gmail.com>
2026-07-28 16:49:33 +0530

    updated added medicine function

--- medicines.py
+++ medicines.py
@@ -16,9 +16,15 @@ def add_medicine():
     medicine_name = input("Enter medicine name: ")
     medicine_quantity = input("Enter medicine quantity: ")
     medicine_expiry_date = input("Enter the expiry date: ")
+    number_of_times = int(input("How many times per day? "))
+    medicine_time = []
+    for time in range(number_of_times):
+        enter_time = input("Enter time")
+        medicine_time.apend(enter_time)
     return {
         "medicine_name": medicine_name,
         "medicine_quantity": medicine_quantity,
-        "medicine_expiry_date": medicine_expiry_date
+        "medicine_expiry_date": medicine_expiry_date,
+        "medicine_timings": medicine_time
 
     }
