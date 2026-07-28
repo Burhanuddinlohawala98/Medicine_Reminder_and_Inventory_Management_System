bab136f247759ff3bf66701bf74fb792914ecf19
hiralal <hiralal.joseph@gmail.com>
2026-07-28 15:53:07 +0530

    added medicine functions

--- medicines.py
+++ medicines.py
@@ -11,3 +11,14 @@
 # - Delete medicines.
 # - Check expiry status.
 # - Calculate days until expiry.
+
+def add_medicine():
+    medicine_name = input("Enter medicine name: ")
+    medicine_quantity = input("Enter medicine quantity: ")
+    medicine_expiry_date = input("Enter the expiry date: ")
+    return {
+        "medicine_name": medicine_name,
+        "medicine_quantity": medicine_quantity,
+        "medicine_expiry_date": medicine_expiry_date
+
+    }
